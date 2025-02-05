02c45a8db5bfce9343149d66cc601512e68c9272
Rupam Mondal <rupam8906@gmail.com>
2025-02-06 01:11:14 +0530

    Implement pagination and sorting for channel videos retrieval; enhance response structure with current page and total pages

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -73,14 +73,28 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+    const { page = 1, limit = 10, sortBy = 'createdAt', sortType = 'desc' } = req.query
     if (!isValidObjectId(req.user?._id)) throw new ApiError(400, "not a valid user id")
+
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+
+    if (pageNumber < 1 || limitNumber < 1) {
+        return res
+            .status(400)
+            .json(new ApiResponse(400, 'Page and limit must be positive integers'));
+    }
+
+    const skip = (pageNumber - 1) * limitNumber;
+
+    const totalContent = await Video.countDocuments({owner: new mongoose.Types.ObjectId(req.user?._id)})
 
     const allVideos = await Video.aggregate([
         {
             $match: {
                 owner: new mongoose.Types.ObjectId(req.user?._id)
             }
-        },{
+        }, {
             $lookup: {
                 from: "likes",
                 localField: "_id",
@@ -94,28 +108,38 @@ const getChannelVideos = asyncHandler(async (req, res) => {
                 foreignField: "video",
                 as: "comments"
             }
-        },{
+        }, {
             $addFields: {
                 likes: { $size: { $ifNull: ["$likes", []] } },
-                comments: { $size: { $ifNull: ["$comments", []] }}
+                comments: { $size: { $ifNull: ["$comments", []] } }
             }
-        },{
-            $sort: { createdAt: -1 }
-        },{
+        }, {
+            $sort: { [sortBy]: sortType === 'asc' ? 1 : -1 },
+        }, {
+            $skip: skip,
+        }, {
+            $limit: limitNumber,
+        }, {
             $project: {
                 _id: 1,
                 title: 1,
                 thumbnail: 1,
                 views: 1,
                 duration: 1,
-                isPublished : 1,
+                isPublished: 1,
                 likes: 1,
                 comments: 1,
+                createdAt: 1
             }
         }
     ])
 
-    return res.status(200).json(new ApiResponse(200, allVideos, "All uploaded videos"))
+    return res.status(200).json(new ApiResponse(200,
+        {
+            videos: allVideos,
+            currentPage: pageNumber,
+            totalPages: Math.ceil((totalContent) / limitNumber)
+        }, "All uploaded videos"))
 })
 
 export {
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -163,7 +163,8 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
         const options = {
             httpOnly: true,
-            secure: true
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
         }
 
         const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
