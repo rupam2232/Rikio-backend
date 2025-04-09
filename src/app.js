6c5f2a3b87b713155c3f39479095c0d4353255d9
Rupam Mondal <rupam8906@gmail.com>
2025-04-09 23:54:45 +0530

    Refactor error handling in loginUser and update ApiError data assignment

--- src/app.js
+++ src/app.js
@@ -60,6 +60,14 @@ app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 app.use("/api/v1/otp", otpRouter)
 
+app.use((err, req, res, next) => {
+  console.error("Error:", err);
 
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+    errors: err.errors || [],
+  });
+});
 
 export {app}
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -84,32 +84,32 @@ const loginUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body
 
 
-    if (!(username || email)){
-        // let variable = new ApiError(400, "username or email is required")
-        // console.log(variable)
-        return res.status(400).json(new ApiResponse(
-            400, {}, "username or email is required"
-        ))
-    }
+    // if (!(username || email)){
+    //     // let variable = new ApiError(400, "username or email is required")
+    //     // console.log(variable)
+    //     return res.status(400).json(new ApiResponse(
+    //         400, {}, "username or email is required"
+    //     ))
+    // }
 
-    // if (!(username || email)) throw new ApiError(400, "username or email is required")
+    if (!(username || email)) throw new ApiError(400, "username or email is required")
 
     const user = await User.findOne({
         $or: [{ username }, { email }]
     })
 
     if (!user){ 
-        return res.status(400).json(new ApiResponse(
-        400, {}, "Invalid user credentials"
-    ))
+        // return res.status(400).json(new ApiResponse(
+        // 400, {}, "Invalid user credentials"))
+        throw new ApiError(400, "Invalid user credentials")
 }
 
     const isPasswordValid = await user.isPasswordCorrect(password)
 
     if (!isPasswordValid){
-        return res.status(400).json(new ApiResponse(
-        400, {}, "Invalid user credentials"))
-        //  throw new ApiError(200, "Invalid user credentials")
+        // return res.status(400).json(new ApiResponse(
+        // 400, {}, "Invalid user credentials"))
+         throw new ApiError(400, "Invalid user credentials")
         }
 
     const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
